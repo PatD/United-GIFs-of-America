d81dcba410d2ca21119d5d0c284d00bdb288cbe4
Patrick Doran <pdoran@gmail.com>
2017-01-09 21:22:30 -0500

    Local storage
    loads local storage state on page load

--- united-gifs-of-america.js
+++ united-gifs-of-america.js
@@ -1,12 +1,32 @@
-// Save to local storage links
+// Save Button in Modal
 var saveButton = document.getElementById("myOfficialStateSaver");
+
+// location of SVG map
+const svgamericamap = document.getElementById("SVGAMERICA");
+
+// State name text node
+var stateNameHeader = document.getElementById("stateNameh2");
+
+// Image + text where per-state-gif lands
+var stateGifHolder = document.getElementById("stateGifHolder");
+var stateTitleforModal = document.getElementById("stateTitleforModal");
+
+// Loading GIF for modal
+var loadingGif = "loading-america.gif";
+
+
+
+// Default fills color of SVG Map
+var mapColor = "#fff";
+var mapColorHover = "#ff0000"
+var mapColorFavorite = "url(#favoriteStateBackgroundImage";
 
 
 // End user's Favorite State
 // localStorage sets this if the user set one in the last session
-var myState = localStorage.favoriteState;
-console.log("global variable set: " + myState );
-
+var myFavoriteState = localStorage.favoriteState;
+var myFavoriteStateID = localStorage.favoriteStateID
+//console.log("global variable set: " + myFavoriteState + myFavoriteStateID);
 
 
 // Helper function, checks to see if LocalStorage is?
@@ -38,6 +58,11 @@ function savemyState(passedStateName, passedStateID){
   
     // Adds event listener for clicking save
     saveButton.addEventListener("click", function handler(){
+      
+      // Remove any previously selected favorite state
+      var oldFavoriteStateID = localStorage.getItem('favoriteStateID');
+      var oldFavoriteStateNode = document.getElementById(oldFavoriteStateID);
+      oldFavoriteStateNode.setAttribute("fill", mapColor);
     
       // If we leave this line in, we can only use the button once? 
       this.removeEventListener('click', handler);
@@ -46,15 +71,10 @@ function savemyState(passedStateName, passedStateID){
       localStorage.setItem('favoriteState', passedStateName);
       localStorage.setItem('favoriteStateID', passedStateID);
       
-      // The state which we just selected to be favorite
+      // The state which we just selected to be favorite, color it in!
       var mySelectedStateID = document.getElementById(passedStateID);
-      console.log(mySelectedStateID);
-      
-      
-      // sets favorite state
-      mySelectedStateID.setAttribute("fill", "url(#favoriteStateBackgroundImage");
+      mySelectedStateID.setAttribute("fill", mapColorFavorite);
   
-   
     });
   
 };
@@ -62,21 +82,9 @@ function savemyState(passedStateName, passedStateID){
 
 // Checks to see if there is anything in localStorage, and acts on it
 function onLoadState(){
-  
-    /*
-    myState = localStorage.favoriteState;
-    
-    console.log("local mystate var: " + myState)
-  
-  
-    return function(){
-         myState = localStorage.favoriteState; 
-    };
-    */
 
-  // find the localstoraged state
-  
-  // set the url(#favoriteStateBackgroundImage)
+  var oldFavoriteStateNode = document.getElementById(myFavoriteStateID);
+  oldFavoriteStateNode.setAttribute("fill", mapColorFavorite);
   
 };
 
@@ -90,24 +98,10 @@ function onLoadState(){
     // Local Storage Getterer
     onLoadState();
     
-    // location of SVG map
-    const svgamericamap = document.getElementById("SVGAMERICA");
-    
-    // State name text node
-    var stateNameHeader = document.getElementById("stateNameh2");
-    
-    // Image + text where per-state-gif lands
-    var stateGifHolder = document.getElementById("stateGifHolder");
-    var stateTitleforModal = document.getElementById("stateTitleforModal");
-    
-    // Loading GIF for modal
-    var loadingGif = "loading-america.gif";
   
-    // the states
+    // Selects all the child nodes of SVGMAP
     var mapchild = svgamericamap.children;
     
-    
-
     // Loop through the states
     for(var i = 0; i < mapchild.length; i++){
 
@@ -115,7 +109,7 @@ function onLoadState(){
       mapchild[i].addEventListener("mouseover", function(){
         
           // Sets background color of state
-          this.setAttribute("fill","red");
+          this.setAttribute("fill",mapColorHover);
           
           var stateFullName = this.getAttribute("data-name");
           
@@ -125,7 +119,6 @@ function onLoadState(){
           // Clears already loaded GIF if there is one
           // So it doesn't show up accidentally for fast-clickers
           stateGifHolder.setAttribute("src", loadingGif);
-
         
       }); // End mouseover function
       
@@ -138,12 +131,12 @@ function onLoadState(){
       
     // Mouseout event handler
       mapchild[i].addEventListener("mouseout", function(){
-        this.setAttribute("fill","#fff");
+        this.setAttribute("fill",mapColor);
         
          var stateFullName = this.getAttribute("data-name");
         
-        // removes state name
-        stateNameHeader.textContent = " ";
+        // removes state name from h2
+        stateNameHeader.textContent = "";
       }); // End mosueout function
       
       
@@ -151,12 +144,15 @@ function onLoadState(){
       // Event listener for click
        mapchild[i].addEventListener("click", function(){
          
+        // Clears already loaded GIF if there is one
+        // So it doesn't show up accidentally for fast-clickers
+        stateGifHolder.setAttribute("src", loadingGif);
+         
         // What the state and ID are here:
         var stateFullName = this.getAttribute("data-name");
         var stateID = this.getAttribute("data-id");
          
-        this.setAttribute("fill","blue");
-          
+        this.setAttribute("fill",mapColorHover);
           
           stateNameHeader.textContent = stateFullName;
         
@@ -197,8 +193,7 @@ function onLoadState(){
           
           // Actual GIF
           var mainGif = returnedGifs.data[0].images.original.url;
-          
-          
+        
           
           // Title in modal:
           stateTitleforModal.innerHTML=stateFullName;
